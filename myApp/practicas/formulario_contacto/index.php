fbbb0c0b8428640fde2faf84a34b08ee2b86ecef
aremtg <tgz57031@gmail.com>
2023-09-14 21:10:28 -0500

    formulario  asico enviar email

--- myApp/practicas/formulario_contacto/index.php
+++ myApp/practicas/formulario_contacto/index.php
@@ -1,8 +1,45 @@
 <?php 
+$errores = '';
+$enviado = '';
 if(isset($_POST['submit'])){
     $nombre = $_POST['nombre'];
     $correo = $_POST['correo'];
     $mensaje = $_POST['mensaje'];
+
+    if(!empty($nombre)){
+        // elimina espacios al final e inicio
+        $nombre = trim($nombre);
+        // sanar, eliminar caracteres especiales
+        $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+        
+    }else{
+        $errores .= 'Por favor ingrese un nombre <br/>';
+    }
+    if(!empty($correo)){
+        $correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+       if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+        $errores .= 'Ingresa un correo valido';
+        }
+    }else{
+        $errores .= 'Por favor ingresa un correo <br/>';
+    }
+
+    if(!empty($mensaje)){
+        $mensaje = htmlspecialchars($mensaje);
+        $mensaje = trim($mensaje);
+        $mensaje = stripcslashes($mensaje);
+    }else{
+        $errores .= 'Por favor escribe un mensaje';
+    }
+
+    if(!$errores){
+        $enviar_a = 'uncorreo@gmail.com';
+        $asunto = 'Correo enviado desde mi pagina';
+        $mensaje_preparado = "De: $nombre \n";
+        $mensaje_preparado .= "Correo: $correo \n";
+        $mensaje_preparado .= "Mensaje: " .$mensaje;
+        $enviado = true;
+    }
 }
 require 'index.view.php';
 ?>
--- myApp/practicas/formulario_contacto/index.view.php
+++ myApp/practicas/formulario_contacto/index.view.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,15 +10,21 @@
     <div class="wrap">
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
         <h1 class="title">Obal</h1>
-            <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre">
-            <input type="email" class="form-control" name="correo" id="correo" placeholder="Correo electronico">
-            <textarea id="mensaje" class="form-control" name="mensaje"  placeholder="Mensaje..."></textarea>
-            <!-- <div class="alert error">
-                Algo salio mal
-            </div>
-            <div class="alert exito">
-                Enviado con exito
-            </div> -->
+            <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre" value="<?php if(!$enviado && isset($nombre))echo $nombre?>">
+            <input type="text" class="form-control" name="correo" id="correo" placeholder="Correo electronico" value="<?php if(!$enviado && isset($correo))echo $correo?>">
+            <textarea id="mensaje" class="form-control" name="mensaje" placeholder="Mensaje..." value="<?php if(!$enviado && isset($mensaje))echo $mensaje?>"></textarea>
+
+            <?php if(!empty($errores)):?>
+                <div class="alert error">
+                    <?php echo $errores; ?>
+                </div>
+            <?php elseif($enviado):?>
+                <div class="alert exito">
+                    <p>Enviado correctamente, <?php echo $mensaje_preparado; ?></p>
+                </div>
+            <?php 
+                endif
+            ?>
             <input type="submit" class="btn-enviar" value="Enviar correo" name="submit">
         </form>
     </div>
